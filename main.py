9f5eb9c443bcc07c9dd91b249a690db1558bb0e1
Santiago Gómez Vilar <santii810@hotmail.com>
2019-05-05 02:22:12 +0200

    Adds datastore example

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
+import jinja2
 import webapp2
-from webapp2_extras import jinja2
+from google.appengine.ext import ndb
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=["jinja2.ext.autoescape"],
+    autoescape=True)
+
+
+class Salute(ndb.Model):
+    name = ndb.StringProperty(required=True)
+    time = ndb.DateTimeProperty(auto_now_add=True)
 
 
 class SaluteHandler(webapp2.RequestHandler):
 
-    def load_input(self):
-        self.name = self.request.get("name", "anonymous")
+    def get_input(self):
+        self.name = self.request.get("name", "pobrecito hablador")
 
     def post(self):
-        self.load_input()
-        print(self.name)
-        #self.name = self.name[0].upper()
+        self.get_input()
+
+        # Get all previous answers
+        salutations = Salute.query().order(Salute.time);
+        # Store the answer
+        salute = Salute(name=self.name);
+        salute.put();
+        # Prepare the answer
         template_values = {
             'name': self.name,
+            'salutations': salutations,
         }
-
-        jinja = jinja2.get_jinja2(app=self.app)
-        self.response.write(
-            jinja.render_template("answer.html", **template_values));
+        template = JINJA_ENVIRONMENT.get_template("templates/answer.html")
+        self.response.write(template.render(template_values));
 
 
-app = webapp2.WSGIApplication([('/hi', SaluteHandler)], debug=True)
+app = webapp2.WSGIApplication([
+    ('/hi', SaluteHandler),
+], debug=True)
